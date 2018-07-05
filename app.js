d1626a191f17f3ff8c98d1c351b859a36a382a7c
SanjanaVarabhi <40849436+SanjanaVarabhi@users.noreply.github.com>
2018-07-05 16:49:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+
+    var app=angular.module("myApp",[]);
+app.controller("myCtr",function($scope,$http){
+    $http.get("data.json").then(function(item){
+    $scope.players=item.data;
+
+    $scope.addUsers=function(){
+        $scope.players.push($scope.players);  
+        
+
+    }
+
+    $scope.removeUser=function(removedUser){
+        $scope.players.splice(removedUser,1);
+       
+            
+        
+ };
+    });
+});
+
